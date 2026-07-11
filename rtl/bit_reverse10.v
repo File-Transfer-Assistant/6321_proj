bb45d98c9f7be79854e29f4cc87cf8cdd9cf0a53
cs4634 <cs4634@columbia.edu>
2026-07-11 10:15:18 -0400

    Create bit_reverse10 module for 10-bit input
    Added a module to reverse a 10-bit input index.

--- rtl/bit_reverse10.v
+++ rtl/bit_reverse10.v
@@ -1,1 +1,17 @@
-
+module bit_reverse10 (
+    input  wire [9:0] in_idx,
+    output wire [9:0] out_idx
+);
+    assign out_idx = {
+        in_idx[0],
+        in_idx[1],
+        in_idx[2],
+        in_idx[3],
+        in_idx[4],
+        in_idx[5],
+        in_idx[6],
+        in_idx[7],
+        in_idx[8],
+        in_idx[9]
+    };
+endmodule
